613de2adc28117d281b451949ad2b0bbbf7b8413
jromerr <jose.a.romero.199@gmail.com>
2026-04-24 19:41:04 -0700

    Create function for playing one round

--- javascript.js
+++ javascript.js
@@ -17,3 +17,65 @@ function getHumanChoice(){
     let choice = prompt("rock, paper, or scissors?");
     return choice.toLowerCase();
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(compChoice, humanChoice){
+    if(compChoice === "rock"){
+        switch (humanChoice){
+            case "rock":
+                console.log("Tie!");
+                break;
+            case "paper":
+                console.log("You win! Paper beats Rock");
+                humanScore++; 
+                break;
+            case "scissors":
+                console.log("You lose! Rock beats Scissors");
+                computerScore++;
+                break;
+            default:
+                console.log("Something went wrong!")
+                break;
+        }
+    }
+    else if(compChoice === "paper"){
+        switch (humanChoice){
+            case "rock":
+                console.log("You lose! Paper beats Rock");
+                computerScore++;
+                break;
+            case "paper":
+                console.log("Tie!");
+                break;
+            case "scissors":
+                console.log("You win! Scissors beats Paper");
+                humanScore++;
+                break;
+            default:
+                console.log("Something went wrong!")
+                break;
+        }
+    }
+    else if(compChoice === "scissors"){
+        switch (humanChoice){
+            case "rock":
+                console.log("You win! Rock beats Scissors");
+                humanScore++;
+                break;
+            case "paper":
+                console.log("You lose! Scissors beats Paper");
+                computerScore++;
+                break;
+            case "scissors":
+                console.log("Tie!");
+                break;
+            default:
+                console.log("Something went wrong!")
+                break;
+        }
+    }
+    return;
+}
+
